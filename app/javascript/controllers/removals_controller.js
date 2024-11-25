4087413459d2f916244b65d8bea9a4544bb5de2b
Mountain Star <github@mtnstar.net>
2024-11-26 09:13:28 +1300

    Chapter 7: Flash messages with turbo

---
+++ app/javascript/controllers/removals_controller.js
@@ -0,0 +1,8 @@
+//// Connects to data-controller="removals"
+//import { Controller } from "@hotwired/stimulus"
+
+//export default class extends Controller {
+  //remove() {
+    //this.element.remove()
+  //}
+//}
